726cd84e89f0a7622cb899d3e19282a5627f00ba
marvingeerken <134588432+marvingeerken@users.noreply.github.com>
2023-06-01 09:00:36 +0200

    add discovery api code

---
+++ discovery_api/connection_query_sample.py
@@ -0,0 +1,61 @@
+# Sample script to connect to the dbt Cloud Discovery API and query a set of models
+
+import requests
+
+def fetch_api_data(api_url, access_token, query, variables=None):
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "query": query,
+        "variables": variables
+    }
+
+    response = requests.post(api_url, json=data, headers=headers)
+
+    if response.status_code == 200:
+        result = response.json()
+        return result
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return None
+
+
+api_url = "https://metadata.cloud.getdbt.com/graphql"
+access_token = "dbts_foo" # Replace dbt Cloud service token
+job_id = 240681 # Replace dbt Cloud job_id
+
+query = """
+query Models($job_id: Int!) {
+  models(jobId: $job_id) {
+    name
+    tests {
+      columnName
+      dependsOn
+    }
+  }
+}
+"""
+
+variables = {
+    "jobid": job_id
+}
+
+data = fetch_api_data(api_url, access_token, query, variables)
+
+if data:
+
+    # Filter models based on "tests" that have "dependsOn" value of "test_relationships"
+    deps_list = []
+    for test in data["data"]["models"]:
+        tests = test.get("tests", [])
+        for test in tests:
+            depends_on = test.get("dependsOn", [])
+            if "macro.dbt.test_relationships" in depends_on:
+                deps_list.append((depends_on[0],depends_on[1],test.get("columnName", "")))
+                break
+
+    deps_list = list(set(deps_list))
+    print(deps_list)
